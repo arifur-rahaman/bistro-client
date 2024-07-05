2a04adb5d7820a0e710e8cff017bd58e36e141c4
unknown <JNKarif>
2024-07-05 20:10:31 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>This is my new project</p>
+      <button className="btn bg-gray-800 w-64 rounded-full">Button</button>
+      <button className="btn bg-red-800 w-64 rounded-full">Button</button>
+      <button className="btn w-64 rounded-full">Button</button>
+      <button className="btn w-64 rounded-full">Button</button>
+    </>
+  );
+}
+
+export default App;
